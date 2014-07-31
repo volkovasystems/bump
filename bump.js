de7d5d69f78b5f17c15d2fa4f60f12b30fc2978f
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-07-31 18:12:00 +0800

    Finished bump procedure.

---
+++ bump.js
@@ -0,0 +1,73 @@
+/*:
+	@module-configuration:
+		{
+			"packageName": "bump",
+			"fileName": "bump.js",
+			"moduleName": "bump",
+			"authorName": "Richeve S. Bebedor",
+			"authorEMail": "richeve.bebedor@gmail.com",
+			"repository": "git@github.com:volkovasystems/bump.git",
+			"testCase": "bump-test.js",
+			"isGlobal": true
+		}
+	@end-module-configuration
+
+	@module-documentation:
+
+	@end-module-documentation
+*/
+var bump = function bump( method, URL, catcher ){
+	/*:
+		@meta-configuration:
+			{
+				"method:required": "string",
+				"URL:required": "string",
+				"catcher:required": "function"
+			}
+		@end-meta-configuration
+	*/
+
+	var request;
+	if( "XMLHttpRequest" in window ){
+		request = new XMLHttpRequest( );
+
+	}else if( "ActiveXObject" in window ){
+		try{
+			request = new ActiveXObject( "Msxml2.XMLHTTP" );
+		}catch( error ){
+			console.warn( "Msxml2.XMLHTTP is not supported trying Microsoft.XMLHTTP" );
+			console.error( error );
+
+			try{
+				request = new ActiveXObject( "Microsoft.XMLHTTP" );
+			}catch( error ){
+				console.warn( "Microsoft.XMLHTTP is not supported" );
+				console.error( error );
+
+				error = new Error( "fatal:procedure lacks the required support" );
+				console.error( error );
+				throw error;
+			}
+		}
+	}
+
+	request.onreadstatechange = function onReadyStateChange( ){
+		var parameterList = Array.prototype.slice.call( arguments );
+
+		parameterList.splice( 0, 1, null );
+
+		catcher.apply( request, parameterList );
+	};
+
+	request.open( method, URL );
+
+	try{
+		request.send( );
+		
+	}catch( error ){
+		console.error( error );
+		catcher.call( request, error );
+	}
+
+	return request;
+};
